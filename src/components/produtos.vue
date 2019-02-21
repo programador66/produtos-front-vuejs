7baae9498a8876b52e51fb1ff96c833e9f9305b9
Caio Cesar <kaio.cesr@gmail.com>
2019-02-21 17:01:06 -0400

    full

--- src/components/produtos.vue
+++ src/components/produtos.vue
@@ -1,5 +1,33 @@
 <template>
-  <div id>teste cad produtos</div>
+  <div id="form">
+    <v-form v-model="valid">
+      <v-container>
+        <v-flex xs12 md4>
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 md4>
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs4 md4>
+          <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+        </v-flex>
+      </v-container>
+    </v-form>
+  </div>
 </template>
 
 
@@ -20,4 +48,7 @@ export default {
 </script>
 
 <style>
+#form {
+  margin: 15%;
+}
 </style>
